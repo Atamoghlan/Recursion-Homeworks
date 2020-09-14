83a253814e59455585a38540af2d54ac841b25fd
Atamoghlan <atamoghlan.a.aliyev@gmail.com>
2020-09-14 21:01:23 +0400

    saved

--- Рекурсия 2/Program.cs
+++ Рекурсия 2/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace Рекурсия_2
+namespace Рекурсия_и_методы_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = AskNumber();
+            int num2 = AskNumber();
+            Console.WriteLine($"НОД равен: {NOD(num1, num2)}");
+        }
+        static int AskNumber()
+        {
+            int a;
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                bool test = int.TryParse(Console.ReadLine(), out a);
+                if (test == false)
+                {
+                    Console.WriteLine("Ошибка! Введите число");
+                }
+                else
+                    return a;
+            }
+        }
+        static int NOD(int num1, int num2)
+        {
+            if (num2 == 0)
+                return num1;
+            if (num1 == 0)
+                return num2;
+            else
+                return NOD(num2, num1 % num2);
         }
     }
 }
--- Рекурсия/Program.cs
+++ Рекурсия/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace Рекурсия
+namespace Рекурсия_и_методы_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double s = 10;
+            Console.WriteLine(Summ(s));
+            Console.ReadKey();
+        }
+        static double Summ(double s)
+        {
+            double p = Pow(s);
+            if (s == 2)
+                return 4;
+            else
+                return p + Summ(s - 1);
+        }
+        static double Pow(double s)
+        {
+            int p = 1;
+            for (int i = 1; i <= s; i++)
+            {
+                p *= 2;
+            }
+            return p;
         }
     }
 }
